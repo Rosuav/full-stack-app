a46c38ec3b08cd145acb09eb8dbf8f50fb58d8c0
Chris Angelico <rosuav@gmail.com>
2016-11-12 09:13:33 +1100

    Add React to the front end

--- client/index.js
+++ client/index.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainComponent from './main-component';
+
+document.addEventListener('DOMContentLoaded', () =>
+	ReactDOM.render(<MainComponent />, document.getElementById('app'))
+);
---
+++ client/main-component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function MainComponent(props) {
+	return <div>
+		<h1>React demo</h1>
+		<p>This is where your content goes.</p>
+	</div>
+};
