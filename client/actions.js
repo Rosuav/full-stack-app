6b1fd479dd925cd6cc39fe843cb7a562a1febf62
Chris Angelico <rosuav@gmail.com>
2016-12-16 12:08:29 +1100

    Switch syntax for brevity

--- client/actions.js
+++ client/actions.js
@@ -1,18 +1,12 @@
-export function spamify_spam() {
-	return {
-		type: 'SPAMIFY_SPAM',
-	};
-}
+export const spamify_spam = () => ({
+	type: 'SPAMIFY_SPAM',
+})
 
-export function hamify_spam() {
-	return {
-		type: 'HAMIFY_SPAM',
-	};
-}
+export const hamify_spam = () => ({
+	type: 'HAMIFY_SPAM',
+})
 
-export function replace_paula(new_paula) {
-	return {
-		type: 'REPLACE_PAULA',
-		paula: new_paula,
-	};
-}
+export const replace_paula = (new_paula) => ({
+	type: 'REPLACE_PAULA',
+	paula: new_paula,
+})
