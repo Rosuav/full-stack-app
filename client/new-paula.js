d497926e4a55d4b71c997cab7ebe6a00ee4e7ad9
Chris Angelico <rosuav@gmail.com>
2016-12-16 12:08:20 +1100

    Debug Paula's bean for her

--- client/new-paula.js
+++ client/new-paula.js
@@ -2,16 +2,16 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from './actions';
 
-export default class NewPaula extends React.Component {
+export default connect()(class NewPaula extends React.Component {
 	update(e) {
 		e.preventDefault();
 		this.props.dispatch(actions.replace_paula(this.refs.paula.value));
 	}
 
 	render() {
-		return <form onSubmit={this.update}>
+		return <form onSubmit={this.update.bind(this)}>
 			<input ref="paula" />
 			<input type="submit" value="Replace Paula" />
 		</form>;
 	}
-}
+})
